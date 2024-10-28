46ee000f388aa32eb3f7856716c115424472e675
Developer27149 <564151947@qq.com>
2024-10-28 13:36:12 +0800

    Initial commit

---
+++ apps/download-demo-monorepo/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="download-demo-monorepo" />
+    </div>
+  );
+}
+
+export default App;
